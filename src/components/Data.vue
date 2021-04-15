5fb0c4cb30f77302b29c47e5ed7f9d3d71673234
Piotr Adamowicz <adamowicz.piotr2@gmail.com>
2021-04-15 16:53:33 +0200

    Lint configured with Vetur

---
+++ src/components/Data.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>{{ msg }}</div>
+</template>
+
+<script>
+export default {
+  name: "Data",
+  data: () => {
+    return {
+      msg: "wooooohooo",
+    };
+  },
+};
+</script>
+
+<style lang="scss" scoped></style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,17 +1,20 @@
 <template>
   <div class="home">
     <HelloWorld msg="Welcome to Your Vue.js App" />
+    <Data />
   </div>
 </template>
 
 <script>
-  // @ is an alias to /src
-  import HelloWorld from "@/components/HelloWorld.vue";
+// @ is an alias to /src
+import HelloWorld from "@/components/HelloWorld.vue";
+import Data from "@/components/Data.vue";
 
-  export default {
-    name: "Home",
-    components: {
-      HelloWorld,
-    },
-  };
+export default {
+  name: "Home",
+  components: {
+    HelloWorld,
+    Data,
+  },
+};
 </script>
