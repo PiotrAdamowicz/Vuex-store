24ef880688967bf5023fc0ce6f559dcb6732da88
Piotr Adamowicz <adamowicz.piotr2@gmail.com>
2021-04-15 17:07:18 +0200

    Setup Works!

--- src/components/Data.vue
+++ src/components/Data.vue
@@ -3,12 +3,24 @@
 </template>
 
 <script>
+import axios from "axios";
+
+const access_key = "31417285af16596aeed9e5a3d3e2e460";
+const url = "latest";
+
 export default {
   name: "Data",
   data: () => {
     return {
       msg: "wooooohooo",
     };
+  },
+  mounted() {
+    axios
+      .get(`http://api.exchangeratesapi.io/v1/${url}?access_key=${access_key}`)
+      .then((res) => {
+        console.log(res.data);
+      });
   },
 };
 </script>
